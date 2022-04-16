ab811b356382be795d5b4e9b260805fe028c5361
Its-me-a-guy <102881579+Its-me-a-guy@users.noreply.github.com>
2022-04-16 13:59:31 -0300

    Update MusicBeatState.hx

--- source/MusicBeatState.hx
+++ source/MusicBeatState.hx
@@ -74,7 +74,7 @@ class MusicBeatState extends FlxUIState
 
 		androidc.visible = false;
 
-		if (SONG.song.toLowerCase() == 'extermination')
+		if (PlayState.SONG.song.toLowerCase() == 'extermination')
 		{
 			_virtualpad = new FlxVirtualPad(NONE, A, 0.75, true);
 			_virtualpad.cameras = [camcontrol];
